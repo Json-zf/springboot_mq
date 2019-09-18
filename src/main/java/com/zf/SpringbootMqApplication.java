770b5d6450b155cf1ee1493ebeaafd6412976ebe
sunshine <365228683@qq.com>
2019-09-18 21:10:44 +0800

    修改主启动类 1.新增了中文注释 2.新增了方法test

--- src/main/java/com/zf/SpringbootMqApplication.java
+++ src/main/java/com/zf/SpringbootMqApplication.java
@@ -3,11 +3,18 @@ package com.zf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 新增中文注释
+ */
 @SpringBootApplication
 public class SpringbootMqApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootMqApplication.class, args);
+    }
+
+    public void test(){
+        System.out.println("张峰你好");
     }
 
 }
